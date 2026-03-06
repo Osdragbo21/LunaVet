26dac966716cd185dfbfa974a2599e87e4fe206b
Osdragbo21 <osvaldo.drago.21@gmail.com>
2026-03-06 03:16:45 -0600

    Primera vista al dashboard

--- src/router/app.router.tsx
+++ src/router/app.router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import { HomePage } from "../pages/home/HomePage";
 import { LoginPage } from "../pages/login/LoginPage";
+import { DashboardPage } from "../pages/dashboard/DashboardPage";
 
 export const appRouter = createBrowserRouter([
     {
@@ -10,5 +11,9 @@ export const appRouter = createBrowserRouter([
     {
         path: "/login",
         element: <LoginPage />,
+    },
+    {
+        path: "/dashboard",
+        element: <DashboardPage />,
     },
 ]);
